edec047c7712f4fab8205d91d2683aa907d14566
Soup2313 <dal636534@UTDallas.edu>
2026-09-16 01:39:30 +0000

    Functions added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
 // main.js
 
-console.log("Hello, World!");
+let targetElement = document.querySelector("h2");
+
+targetElement.addEventListner("mouseover", function() {
+   this.innerText = "TIME";
+   this.style = "cursor: wait";
+});
+
+targetElement.addEventListner("mouseout", function() {
+   this.innerText = "IKEA";
+   this.style = "cursor: pointer";
+});
